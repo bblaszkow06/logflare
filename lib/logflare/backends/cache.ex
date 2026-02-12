12ea949386e976f2e7ab0bf5cdf551347842f350
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2026-02-12 09:56:06 +0100

    Introduce cache for fetching by token

--- lib/logflare/backends/cache.ex
+++ lib/logflare/backends/cache.ex
@@ -31,7 +31,38 @@ defmodule Logflare.Backends.Cache do
   end
 
   def list_backends(arg), do: apply_repo_fun(__ENV__.function, [arg])
+
+  @doc """
+  Clears cached `list_backends` queries for a specific source.
+  """
+  @spec clear_list_backends(source_id :: integer()) :: :ok
+  def clear_list_backends(source_id) when is_integer(source_id) do
+    Cachex.del(__MODULE__, {:list_backends, [[source_id: source_id]]})
+    :ok
+  end
+
   def get_backend(arg), do: apply_repo_fun(__ENV__.function, [arg])
+  def get_backend_by(arg), do: apply_repo_fun(__ENV__.function, [arg])
+
+  @doc """
+  Fechtches backend by token and user id with caching.
+  """
+  @spec fetch_backend_by_token(binary(), integer()) :: {:ok, Backend.t()} | {:error, :not_found}
+  def fetch_backend_by_token(token, user_id) do
+    case get_backend_by(token: token, user_id: user_id) do
+      nil -> {:error, :not_found}
+      backend -> {:ok, backend}
+    end
+  end
+
+  @doc """
+  Busts entries created by `fetch_backend_by_token/2`
+  """
+  @spec clear_backend_by_token(binary(), integer()) :: :ok
+  def clear_backend_by_token(token, user_id) do
+    Cachex.del(__MODULE__, {:get_backend_by, [[token: token, user_id: user_id]]})
+    :ok
+  end
 
   defp apply_repo_fun(arg1, arg2) do
     Logflare.ContextCache.apply_fun(Backends, arg1, arg2)
